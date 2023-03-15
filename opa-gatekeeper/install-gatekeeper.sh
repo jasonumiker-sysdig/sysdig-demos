780c29d617690be5f8552bde0626e86e5040f2a3
Jason Umiker <jason.umiker@sysdig.com>
2023-03-15 17:49:55 +1100

    Removing Gatekeeper example

--- opa-gatekeeper/install-gatekeeper.sh
+++
@@ -1,5 +0,0 @@
-helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
-helm install -n gatekeeper-system gatekeeper gatekeeper/gatekeeper --create-namespace
-kubectl apply -k ./policies/constraint-templates
-sleep 5
-kubectl apply -k ./policies/constraints

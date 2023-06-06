af5e29cd070c6c678a3a32c1aec810be08f2a1c5
Jason Umiker <jason.umiker@sysdig.com>
2023-06-06 12:50:26 +1000

    Added monitor integrations for MySQL and Oracle

---
+++ demos/data-exfil-mysql/deploy-integration.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl create secret -n mysql-sakila generic mysql-exporter --from-file=.my.cnf=./mysql-exporter.cnf
+helm install -n mysql-sakila -f values.yaml --repo https://sysdiglabs.github.io/integrations-charts mysql-sakila-mysql-sakila-deployment mysql-exporter

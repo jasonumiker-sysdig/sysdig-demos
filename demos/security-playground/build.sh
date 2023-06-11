1d3f5458af35dcc7bc31e6c8c9cafc5abf2fccb7
Jason Umiker <jason.umiker@sysdig.com>
2023-06-11 16:30:58 +1000

    Updating security-playground

--- demos/security-playground/build.sh
+++ demos/security-playground/build.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag jasonumiker/security-playground:190523 \
+  --tag jasonumiker/security-playground:110623 \
   .
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \

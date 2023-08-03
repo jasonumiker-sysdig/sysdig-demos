50cf135035b21d30bce6c95d242f3786f582426a
Jason Umiker <jason.umiker@sysdig.com>
2023-08-03 22:14:55 +1000

    Updated sysdig agent

--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 VM_NAME="microk8s-vm-sysdig"
 SETUP_DIR=$(pwd)
-LBIPS="192.168.0.20-192.168.0.29"
 
 # You can reset things back to defaults by deleting the VM and then let the script recreate
 multipass delete $VM_NAME
 multipass purge
 
 # Provision your local cluster VM
-multipass launch --cpus 6 --memory 8G --disk 20G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 --bridged 22.04
+#multipass launch --cpus 6 --memory 8G --disk 20G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 --bridged 22.04
+multipass launch --cpus 6 --memory 8G --disk 20G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 22.04
 
 # Copy the .kube/config to the local machine
 cd ~/.kube
 multipass transfer $VM_NAME:/home/ubuntu/.kube/config .
-
-# Enable MetalLB in the LBIPS range
-multipass exec $VM_NAME -- sudo microk8s enable metallb:$LBIPS
 
 # Deploy Sysdig Agent
 cd $SETUP_DIR

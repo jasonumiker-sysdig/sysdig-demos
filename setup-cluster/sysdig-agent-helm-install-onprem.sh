21926805eecad276ad3d30ccff360a93df7f15cb
Jason Umiker <--help>
2023-08-14 11:36:03 +1000

    Made scripts executable

---
+++ setup-cluster/sysdig-agent-helm-install-onprem.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+kubectl create ns sysdig-agent
+helm repo add sysdig https://charts.sysdig.com
+helm repo update
+helm install sysdig-agent --namespace sysdig-agent -f sysdig-agent-values.yaml.onprem sysdig/sysdig-deploy --version 1.15.6

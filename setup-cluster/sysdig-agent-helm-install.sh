45de1e6909a380d2c7886c9589a699d98718ce00
Jason Umiker <--help>
2023-09-07 13:47:27 +1000

    Updated sysdig-agent to the latest

--- setup-cluster/sysdig-agent-helm-install.sh
+++ setup-cluster/sysdig-agent-helm-install.sh
@@ -2,4 +2,4 @@
 kubectl create ns sysdig-agent
 helm repo add sysdig https://charts.sysdig.com
 helm repo update
-helm install sysdig-agent --namespace sysdig-agent -f sysdig-agent-values.yaml sysdig/sysdig-deploy --version 1.19.3
+helm install sysdig-agent --namespace sysdig-agent -f sysdig-agent-values.yaml sysdig/sysdig-deploy --version 1.22.3

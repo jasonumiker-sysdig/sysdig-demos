5a890ab9685532234e35ef5ecd6f3c9534801810
Jason Umiker <jason.umiker@sysdig.com>
2023-09-11 22:30:18 +1000

    Updated Sysdig Helm Chart

--- setup-cluster/sysdig-agent-helm-install.sh
+++ setup-cluster/sysdig-agent-helm-install.sh
@@ -2,4 +2,4 @@
 kubectl create ns sysdig-agent
 helm repo add sysdig https://charts.sysdig.com
 helm repo update
-helm install sysdig-agent --namespace sysdig-agent -f sysdig-agent-values.yaml sysdig/sysdig-deploy --version 1.22.4
+helm install sysdig-agent --namespace sysdig-agent -f sysdig-agent-values.yaml -f promscrape-values.yaml sysdig/sysdig-deploy

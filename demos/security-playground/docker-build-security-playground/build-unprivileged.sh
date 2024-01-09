ea753cd081c9595e56a4db4139f9c8e913c6d6c2
Jason Umiker <jason.umiker@sysdig.com>
2024-01-09 15:35:47 +1100

    Updated apps to new builds

--- demos/security-playground/docker-build-security-playground/build-unprivileged.sh
+++ demos/security-playground/docker-build-security-playground/build-unprivileged.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-docker buildx create --name mybuilder --bootstrap --use
-docker buildx build --push --file ./Dockerfile-unprivileged \
+docker buildx create --name mybuilder --bootstrap --use 
+docker buildx build --file ./Dockerfile-unprivileged --push \
   --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:270723 \
+  --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:070124 \
   .
-docker buildx build --push --file ./Dockerfile-unprivileged \
-  --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:latest \
-  .
+#docker buildx build --push \
+#  --platform linux/arm64,linux/amd64 \
+#  --tag public.ecr.aws/m9h2b5e7/security-playground-unprivileged:latest \
+#  .
 docker buildx rm mybuilder

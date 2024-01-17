686a469b9cb7e694b5922859f95880139d7f9c07
Jason Umiker <jason.umiker@sysdig.com>
2024-01-17 12:52:40 +1100

    Fixed typo

--- demos/security-playground/run-tests.sh
+++ demos/security-playground/run-tests.sh
@@ -4,7 +4,7 @@ set +e
 ./example-curls.sh
 ./example-curls-restricted.sh
 ./example-curls-nodrift.sh
-./example-curls-nomalware
+./example-curls-nomalware.sh
 kubectl delete pods --all -n security-playground
 kubectl delete pods --all -n security-playground-restricted
 kubectl delete pods --all -n security-playground-nodrift

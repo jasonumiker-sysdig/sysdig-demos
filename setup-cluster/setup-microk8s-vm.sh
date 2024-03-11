985be7ef3dd3eb4b8956abedd59a7683056498c8
Jason Umiker <jason.umiker@sysdig.com>
2024-03-11 16:01:52 +1100

    Un-bridged the network

--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -3,12 +3,11 @@ VM_NAME="microk8s-vm-sysdig"
 SETUP_DIR=$(pwd)
 
 # You can reset things back to defaults by deleting the VM and then let the script recreate
-multipass delete $VM_NAME
-multipass purge
+multipass delete $VM_NAME --purge
 
 # Provision your local cluster VM
-multipass launch --cpus 8 --memory 16G --disk 30G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 --bridged 22.04
-#multipass launch --cpus 8 --memory 16G --disk 30G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 22.04
+#multipass launch --cpus 8 --memory 16G --disk 30G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 --bridged 22.04
+multipass launch --cpus 8 --memory 16G --disk 30G --name $VM_NAME --cloud-init cloud-init.yaml --timeout 600 22.04
 
 # Copy the .kube/config to the local machine
 cd ~
